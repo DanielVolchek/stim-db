92df071f5eef031fa95cb5b092344ff4bb359b62
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-01 15:21:15 -0400

    added middleware file

--- src/router/middleware/middleware.go
+++ src/router/middleware/middleware.go
@@ -7,15 +7,32 @@ import (
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("cookies are ", r.Cookies())
+		fmt.Println("in session")
+		// fmt.Println("cookies are ", r.Cookies())
 
-		_, err := r.Cookie("session")
+		// _, err := r.Cookie("session")
+		//
+		// if err != nil {
+		// 	w.WriteHeader(http.StatusUnauthorized)
+		// 	w.Write([]byte("Requires session cookie"))
+		// 	return
+		// }
 
-		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Requires session cookie"))
-			return
-		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func AdminMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("in admin")
+
+		// _, err := r.Cookie("admin")
+
+		// if err != nil {
+		// 	w.WriteHeader(http.StatusUnauthorized)
+		// 	w.Write([]byte("Requires session cookie"))
+		// 	return
+		// }
 
 		next.ServeHTTP(w, r)
 	})
