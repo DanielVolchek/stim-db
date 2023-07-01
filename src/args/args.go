3c893afdbfd91bd9092ef1098d43996f60fc067e
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-01 15:21:02 -0400

    added PORT arg

--- src/args/args.go
+++ src/args/args.go
@@ -1,9 +1,9 @@
-package args;
+package args
 
 import (
-  "errors"
-  "fmt"
-  "os"
+	"errors"
+	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -11,13 +11,14 @@ import (
 type EnvArgs struct {
 	DB_URL               string
 	SUPABASE_SERVICE_KEY string
+	PORT                 string
 }
 
-type CmdArgs struct{
-  seed bool
+type CmdArgs struct {
+	Seed bool
 }
 
-func loadEnvVars() (EnvArgs, error) {
+func LoadEnvVars() (EnvArgs, error) {
 	envArgs := EnvArgs{}
 
 	err := godotenv.Load(".env")
@@ -28,6 +29,7 @@ func loadEnvVars() (EnvArgs, error) {
 
 	envArgs.DB_URL = os.Getenv("DB_URL")
 	envArgs.SUPABASE_SERVICE_KEY = os.Getenv("SUPABASE_SERVICE_KEY")
+	envArgs.PORT = os.Getenv("PORT")
 
 	if envArgs.DB_URL == "" {
 		return EnvArgs{}, errors.New("DB_URL not found in .env")
@@ -37,26 +39,22 @@ func loadEnvVars() (EnvArgs, error) {
 		return EnvArgs{}, errors.New("SUPABASE_SERVICE_KEY not found in .env")
 	}
 
-
-
 	return envArgs, nil
-
 }
 
-func loadCmdArgs() CmdArgs{
+func LoadCmdArgs() CmdArgs {
 
-  args := CmdArgs{}
+	args := CmdArgs{}
 
 	if len(os.Args) > 1 {
 		for _, arg := range os.Args[1:] {
 
 			if arg == "--seed" {
-				args.seed = true
+				args.Seed = true
 			}
 		}
 
 	}
 
-  return args;
+	return args
 }
-
