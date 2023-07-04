8c9519c5f0a6ff984a91b5fa999645cfa5b7ab68
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-04 01:32:14 -0400

    added models

--- src/db/models.go
+++ src/db/models.go
@@ -1,14 +1,14 @@
-package db;
+package db
 
 import "time"
 
 type User struct {
-	ID           string      `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Username     string      `gorm:"column:username;unique"`
-	PasswordHash string      `gorm:"column:password_hash"`
-	Sessions     []Session   `gorm:"foreignKey:UserID"`
-	Role         Role        `gorm:"column:role;default:USER;type:role_enum"`
-	RentEvent    *RentEvent  `gorm:"foreignKey:RentedByID"`
+	ID           string     `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
+	Username     string     `gorm:"column:username;unique"`
+	PasswordHash string     `gorm:"column:password_hash"`
+	Sessions     []Session  `gorm:"foreignKey:UserID"`
+	Role         Role       `gorm:"column:role;default:USER;type:role_enum"`
+	RentEvent    *RentEvent `gorm:"foreignKey:RentedByID"`
 }
 
 type Role string
@@ -19,29 +19,29 @@ const (
 )
 
 type Session struct {
-	ID        string      `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Token     string      `gorm:"column:token;unique"`
-	ExpiresAt time.Time   `gorm:"column:expires_at"`
-	UserID    string      `gorm:"column:user_id"`
-	User      User        `gorm:"foreignKey:UserID"`
+	ID        string    `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
+	Token     string    `gorm:"column:token;unique"`
+	ExpiresAt time.Time `gorm:"column:expires_at"`
+	UserID    string    `gorm:"column:user_id"`
+	User      User      `gorm:"foreignKey:UserID"`
 }
 
 type Item struct {
-	ID               int         `gorm:"column:id;primaryKey;autoIncrement"`
-	Name             string      `gorm:"column:name"`
-	Desc             string      `gorm:"column:desc"`
-	PurchaseLink     string      `gorm:"column:purchase_link"`
-	Image            *Image      `gorm:"foreignKey:ItemID"`
-	CurrentRentEvent *RentEvent  `gorm:"foreignKey:ItemID"`
+	ID               int        `gorm:"column:id;primaryKey;autoIncrement"`
+	Name             string     `gorm:"column:name"`
+	Desc             string     `gorm:"column:desc"`
+	PurchaseLink     string     `gorm:"column:purchase_link"`
+	Image            *Image     `gorm:"foreignKey:ItemID"`
+	CurrentRentEvent *RentEvent `gorm:"foreignKey:ItemID"`
 }
 
 type RentEvent struct {
-	ID         int        `gorm:"column:id;primaryKey;autoIncrement"`
-	ItemID     int        `gorm:"column:item_id;unique"`
-	Item       Item       `gorm:"foreignKey:ItemID"`
-	RentedByID string     `gorm:"column:rented_by_id;unique"`
-	RentedBy   User       `gorm:"foreignKey:RentedByID"`
-	RentedOn   time.Time  `gorm:"column:rented_on"`
+	ID         int       `gorm:"column:id;primaryKey;autoIncrement"`
+	ItemID     int       `gorm:"column:item_id;unique"`
+	Item       Item      `gorm:"foreignKey:ItemID"`
+	RentedByID string    `gorm:"column:rented_by_id;unique"`
+	RentedBy   User      `gorm:"foreignKey:RentedByID"`
+	RentedOn   time.Time `gorm:"column:rented_on"`
 }
 
 type Image struct {
@@ -50,4 +50,3 @@ type Image struct {
 	ItemID int    `gorm:"column:item_id;unique"`
 	Item   Item   `gorm:"foreignKey:ItemID"`
 }
-
