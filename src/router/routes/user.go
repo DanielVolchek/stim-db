4ba421742f2bf30e87473d0f6f1666892fbb5d3b
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-04 01:32:59 -0400

    started writing user handler

--- src/router/routes/user.go
+++ src/router/routes/user.go
@@ -7,6 +7,13 @@ import (
 )
 
 var userHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "GET" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method " + r.Method + "not allowed"))
+		return
+	}
+
 	w.Write([]byte("good job my friend it is nice to meet you!"))
 })
 
