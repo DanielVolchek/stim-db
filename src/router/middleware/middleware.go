02d30ad08d9bd2687d1e0b513b2cb025832afcae
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-04 01:31:44 -0400

    work on adding auth in

--- src/router/middleware/middleware.go
+++ src/router/middleware/middleware.go
@@ -1,22 +1,47 @@
 package middleware
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"net/http"
+
+	"com.stimstore/stim-db/src/db"
 )
+
+func AuthCore(session *http.Cookie, needsAdmin bool) error {
+
+	err := session.Valid()
+
+	if err != nil {
+		return err
+	}
+
+	user, err := db.AuthenticateUserBySession(session.String())
+
+	if err != nil {
+
+		return err
+	}
+
+	if needsAdmin && user.Role != "ADMIN" {
+		return errors.New("User is unauthorized as an admin")
+	}
+
+	return nil
+}
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("in session")
-		// fmt.Println("cookies are ", r.Cookies())
 
-		// _, err := r.Cookie("session")
-		//
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusUnauthorized)
-		// 	w.Write([]byte("Requires session cookie"))
-		// 	return
-		// }
+		session, err := r.Cookie("session")
+
+		err = AuthCore(session, false)
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			log.Println("Error occurred: ", err.Error())
+			w.Write([]byte("Unauthorized to access this resource given information provided"))
+		}
 
 		next.ServeHTTP(w, r)
 	})
@@ -24,15 +49,16 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 func AdminMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("in admin")
 
-		// _, err := r.Cookie("admin")
+		session, err := r.Cookie("session")
 
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusUnauthorized)
-		// 	w.Write([]byte("Requires session cookie"))
-		// 	return
-		// }
+		err = AuthCore(session, true)
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			log.Println("Error occurred: ", err.Error())
+			w.Write([]byte("Unauthorized to access this resource given information provided"))
+		}
 
 		next.ServeHTTP(w, r)
 	})
