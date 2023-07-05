a3cae376c9335390286cf2a90799ced4b243113f
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-05 11:41:02 -0400

    rename to migrate

--- pkg/db/migrate.go
+++ pkg/db/migrate.go
@@ -9,11 +9,18 @@ func MigrateDB() {
 	// This will probably error but only because type already exists
 	DB_CONN.Exec("CREATE TYPE role_enum AS ENUM ('USER', 'ADMIN')")
 
-	err := DB_CONN.AutoMigrate(&User{}, &Session{}, &Item{}, &RentEvent{}, &Image{})
+	err := DB_CONN.AutoMigrate(
+		&User{},
+		&Session{},
+		&Item{},
+		&RentEvent{},
+		&Image{},
+		&EmailToken{},
+		&ServerAuthToken{},
+	)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	CreateNewSessionOnUser()
 
 }
