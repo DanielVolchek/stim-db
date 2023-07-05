de66fdfb7611afaac6a95082f98d39b8f59241c5
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-05 11:41:10 -0400

    add validateservertoken functionality

--- pkg/db/read.go
+++ pkg/db/read.go
@@ -1,1 +1,28 @@
 package db
+
+import "log"
+
+func ValidateServerToken(token any) bool {
+	var err error
+
+	switch t := token.(type) {
+	case string:
+		err = DB_CONN.First(&ServerAuthToken{Token: t}).Error
+	case ServerAuthToken:
+		err = DB_CONN.First(&t).Error
+	case *ServerAuthToken:
+		err = DB_CONN.First(t).Error
+	default:
+		log.Fatal("typeof token must be string or ServerAuthToken")
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if DB_CONN.RowsAffected == 0 {
+		return false
+	}
+
+	return true
+}
